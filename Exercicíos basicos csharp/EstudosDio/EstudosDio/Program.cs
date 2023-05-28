4f6bb4bbb8e8488ec66afa95ab8de1f4d44bd645
Allan Moura <anuikpom@gmail.com>
2023-05-28 20:03:10 -0300

    Exercicio da hotelaria Dio

---
+++ Exercicíos basicos csharp/EstudosDio/EstudosDio/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using EstudosDio.Models;
+
+namespace EstudosDio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa1 = new Pessoa();
+
+            pessoa1.Idade = 12;
+            pessoa1.Nome = "Jorge";
+
+            pessoa1.Apresentar();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# CSharpCodes
- C# REPOSITORY FOR CODES AND STUFF RELATED THAT I'M STUDYING.
+# csharpStudies
+C# REPOSITORY FOR CODES AND STUFF RELATED THAT I'M STUDYING.
+
+Feel free to use any code here, they are meant to help anyone who need it :).
